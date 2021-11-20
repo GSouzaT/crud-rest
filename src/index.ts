61bb9f46a26666294346a1dbd2a11162a54d95a6
GSouzaT <tsleirbag@gmail.com>
2021-11-19 21:15:57 -0300

    adição de error handler no get by id

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 import statusRoute from './routes/status.route';
 import usersRoute from './routes/users.route';
+import errorHandler from './middlewares/error-handler.middleware';
 
 const app = express();
 
@@ -10,8 +11,10 @@ app.use(express.urlencoded({ extended: true }));
 
 // Configurações de rotas
 app.use(usersRoute);
-
 app.use(statusRoute);
+
+// Configuração de Handlers de erro
+app.use(errorHandler);
 
 app.listen(3000, () => {
     console.log('Running on port 3000');
--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,6 +1,8 @@
+import { error } from 'console';
 import { query } from 'express';
 import db from '../db';
 import User from "../models/user.model";
+import DatabaseError from '../models/errors/database.error.model';
 
 
 
@@ -17,18 +19,23 @@ class UserRepository {
     }
 
     async findById(uuid: string): Promise<User> {
-        const query = `
+        try {
+
+            const query = `
             SELECT uuid, username 
             FROM application_user
             WHERE uuid = $1 
-        `;
-
-        const values = [uuid];
-
-        const { rows } = await db.query<User>(query, values);
-        const [ user ] = rows;
-
-        return user;
+            `;
+            
+            const values = [uuid];
+            
+            const { rows } = await db.query<User>(query, values);
+            const [ user ] = rows;
+            
+            return user;
+        } catch (error) {
+            throw new DatabaseError('Erro na consulta por ID', error);
+        }
     }
 
     async create(user: User): Promise<string> {
